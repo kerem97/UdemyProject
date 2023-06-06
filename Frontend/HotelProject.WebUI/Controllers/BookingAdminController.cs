187c23f71ea61f2974a6d72eee7c225f25f85cb0
Kerem DURAK <kerem.durak@yahoo.com>
2023-06-06 21:48:25 +0300

    updated

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,30 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:53554/api/Booking2");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/BookingDto/ResultBookingDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelProject.WebUI.Dtos.BookingDto
+{
+    public class ResultBookingDto
+    {
+        public int Booking2ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string AdultCount { get; set; }
+        public string ChildCount { get; set; }
+        public string RoomCount { get; set; }
+        public string SpecialRequest { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
