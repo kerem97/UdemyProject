1356d55d558016e3588be4ea42612391860db8c1
Kerem DURAK <kerem.durak@yahoo.com>
2023-06-09 01:54:34 +0300

    updated

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+        public PartialViewResult SideBarAdminContactPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SideBarAdminContactCategoryPartial()
+        {
+            return PartialView();
+        }
+    }
+}
