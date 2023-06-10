ecee8803a11ffab4ad26c03b47d4aa357d30f197
Kerem DURAK <kerem.durak@yahoo.com>
2023-06-10 16:30:12 +0300

    mime kit mail

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("hotelierAdmin", "mail adresi");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("mail", "password key");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+
+            return View();
+        }
+    }
+}
